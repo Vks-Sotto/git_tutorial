bbf508a8793d3e6f8f9a4459e5155422f9820732
Vks-Sotto <ranchodbhakt@gmail.com>
2024-10-05 16:36:41 +0100

    added some comments to the add function

--- math.py
+++ math.py
@@ -1,6 +1,10 @@
 #I need to implement this
 def add(x,y):
-	return x+y
+	#if x and y are both integers, then add them 
+	if (type(x)== int and type(y)== int):
+		return x+y
+	#otherwise return None
+	return none
 
 #Bob needs to implement this
 def mult(x,y):
